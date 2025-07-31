e692a8d8ae47adff584841555de61790c5f04422
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-07-31 22:28:43 +0530

    So much potential on the table

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -7,11 +7,13 @@ public class Movement : MonoBehaviour
     [SerializeField] private float Maxspeed = 0f;
     [SerializeField] private float accelerationConstant = 0f;
     [SerializeField] private float decelerationConstant = 0f;
+    [SerializeField] private float jumpForce = 10f;
 
     private Animator animator;
     private Vector2 velocity = Vector2.zero;
     public Vector2 lastAcceleration = Vector2.zero;
     public bool dirChanged = false;
+    public float upForce = 0f;
 
     private void Awake()
     {
@@ -21,11 +23,24 @@ public class Movement : MonoBehaviour
     private void Update()
     {
         Vector2 horizontalInput = new Vector2(Input.GetAxis("Horizontal"), 0f);
-        Vector2 verticalInput = new Vector2(0f, Input.GetAxis("Vertical"));
+        //Vector2 verticalInput = new Vector2(0f, Input.GetAxis("Vertical"));
+        Vector2 verticalInput = Vector2.zero;
 
         // Acceleration Movement
-        Vector2 movement = horizontalInput;// + verticalInput;
+        Vector2 movement = horizontalInput + verticalInput;
         Vector2 acceleration = movement.normalized;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            upForce = 100f;
+        }
+        else if (upForce > 0f)
+        {
+            upForce -= 10f * Time.deltaTime;
+            Debug.Log("Jump: " + upForce);
+            if(Approximate(upForce, 0f, 0.1f)) upForce = 0f;
+        }
+        acceleration = new Vector2(acceleration.x, upForce);
 
         if (acceleration != Vector2.zero && dirChanged == false)
             velocity += acceleration * accelerationConstant * Time.deltaTime;
@@ -34,13 +49,13 @@ public class Movement : MonoBehaviour
             velocity -= velocity * decelerationConstant * Time.deltaTime;
         }
 
-        if(lastAcceleration != acceleration && acceleration != Vector2.zero)
+        if (lastAcceleration != acceleration && acceleration != Vector2.zero)
         {
             dirChanged = true;
             Debug.Log("RUN");
             velocity -= velocity * decelerationConstant * Time.deltaTime * 2f;
 
-            if(Approximate(velocity, Vector2.zero, 0.1f))
+            if (Approximate(velocity, Vector2.zero, 0.1f))
             {
                 dirChanged = false;
             }
@@ -63,6 +78,11 @@ public class Movement : MonoBehaviour
     bool Approximate(Vector2 a, Vector2 b, float epsilon)
     {
         return Mathf.Abs(a.x - b.x) < epsilon && Mathf.Abs(a.y - b.y) < epsilon;
+    }
+
+    bool Approximate(float a, float b, float epsilon)
+    {
+        return Mathf.Abs(a - b) < epsilon;
     }
 
     void AnimationHandle(Vector2 direction)
